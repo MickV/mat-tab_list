9282961d90afbdbf93c7db6beb83bfc661638e49
Mickael <mickael.varlet@xelians.fr>
2020-06-29 22:18:18 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, HostListener } from '@angular/core';
+import { MatTabGroup, MatTabChangeEvent } from '@angular/material/tabs';
+import { MatRipple } from '@angular/material/core';
+import { MatListOption } from '@angular/material/list';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'list';
+  
+  @ViewChild('demoTab') demoTab: MatTabGroup;
+
+  typesOfShoes: string[] = ['Boots', 'Clogs', 'Loafers', 'Moccasins', 'Sneakers'];
+  titi: string[] = ['titi', 'titi', 'titi', 'titi', 'titi'];
+  toto: string[] = ['toto', 'toto', 'toto', 'toto', 'toto'];
+
+  map = {
+    'typesOfShoes': this.typesOfShoes,
+    'toto': this.toto,
+    'titi': this.titi,
+  }
+
+  @HostListener('document:keydown', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent) {
+      if (event.key === 'ArrowRight') {
+        this.demoTab.selectedIndex++;
+      } else if(event.key === 'ArrowLeft') {
+        this.demoTab.selectedIndex--;
+      }
+  }
+
+  getKey() {
+    return Object.keys(this.map);
+  }
+
+  onEnter(e: any) {
+    console.warn('onEnter: ', e);
+  }
+
+  changeFocus(e: MatTabChangeEvent) {
+    console.warn('changefocus: ', e)
+    document.getElementById('select-' + e.tab.textLabel).focus();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatListModule} from '@angular/material/list';
+import {MatTabsModule} from '@angular/material/tabs';
+import { MatRippleModule } from '@angular/material/core';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatListModule,
+    MatTabsModule,
+    MatRippleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
